d4f82fb794a4ebb76161743f2758c54568625a47
walidsadat <walidsadat@MacBook-Pro-de-Walid-2.local>
2021-11-22 19:03:00 +0100

    backend fixed

--- contracts/BuildCollective.sol
+++ contracts/BuildCollective.sol
@@ -4,42 +4,44 @@ pragma experimental ABIEncoderV2;
 import "./Ownable.sol";
 
 contract BuildCollective is Ownable {
+
+  //Structs
   struct User {
     string username;
     uint256 balance;
     bool registered;
   }
 
-  address[] public userAddresses;
+  address[] internal usersAddresses;
 
   struct Entreprise {
     string name;
     address owner;
-    address[] membersAddresses;
+    address[] members;
     uint256 balance;
   }
 
-  uint256 internal ProjectId;
+  uint256 internal CptProject;
 
   struct Project {
     uint256 id;
     string name;
-    uint256 balance;
-    address[] contributorsAddresses;
     address owner;
-    string belongsTo; // "User" or "Entreprise"
-    string link; //Link to GitHub or GitLab project
+    address[] contributors;
+    uint256 balance;
+    bool belongsToUser;
+    string link;
   }
 
-  uint256 internal BountieId;
+  uint256 internal CptBountie;
 
   struct Bountie {
     uint256 id;
-    uint256 reward;
     address opener;
     address payable solver;
-    string link; //Link to GitHub or GitLab issue
-    bool solved;
+    string link;
+    uint256 reward;
+    bool resolved;
   }
 
   mapping(address => User) private users;
@@ -48,13 +50,25 @@ contract BuildCollective is Ownable {
   mapping(uint => Bountie[]) private bounties;
 
   event UserSignedUp(address indexed userAddress, User indexed user);
-  event EntrepriseSignedUp(address indexed ownerAddress, Entrepreise indexed entreprise);
+  event EntrepriseSignedUp(address indexed ownerAddress, Entreprise indexed entreprise);
   event ProjectCreated(address indexed ownerAddress, Project indexed project);
-  event BountieOpened(address indexed openerAddress, Bountie indexed bountie);
+  event BountieOpened(address indexed openerAddress, Bountie indexed issue);
 
-  //User 
-  function getAllUsers() external view returns (address[] memory){
-    return userAddresses;
+  //User
+  function allUsers() external view returns (address[] memory) {
+    return usersAddresses;
+  }
+
+  function user(address userAddress) public view returns (User memory) {
+    return users[userAddress];
+  }
+
+  function signUp(string memory username) public returns (User memory) {
+    require(bytes(username).length > 0);
+    users[msg.sender] = User(username, 0, true);
+    usersAddresses.push(msg.sender);
+    emit UserSignedUp(msg.sender, users[msg.sender]);
+    return users[msg.sender];
   }
 
   function addBalance(uint256 amount) public returns (bool) {
@@ -63,71 +77,51 @@ contract BuildCollective is Ownable {
     return true;
   }
 
-  function user(address userAddress) public view returns (User memory) {
-    require(users[userAddress].registred);
-    return users[userAddress];
-  }
-
-  function signUp(string memory username, uint256 amount) public returns (User memory) {
-    require(bytes(username).length > 0);
-    users[msg.sender] = User(username, amount, true);
-    emit UserSignedUp(msg.sender, users[msg.sender]);
-    userAddresses.push(msg.sender);
-    return users[msg.sender];
-  }
-
   //Entreprise
   function entreprise(address entrepriseAddress) public view returns (Entreprise memory) {
+    require(users[msg.sender].registered);
     return entreprises[entrepriseAddress];
   }
 
-  function signUpEntreprise(string memory entreprisename, address[] memory members, uint256 balance) public returns (Entreprise memory) {
-    require(users[msg.sender].registred);
-    require(bytes(entreprisename).length > 0);
-    entreprisenames[msg.sender] = Entreprise(entreprisename, msg.sender, members, balance);
+  function signUpEntreprise(string memory name, address[] memory members, uint256 balance) public returns (Entreprise memory) {
+    require(users[msg.sender].registered);
+    require(bytes(name).length > 0);
+    entreprises[msg.sender] = Entreprise(name, msg.sender, members, balance);
     emit EntrepriseSignedUp(msg.sender, entreprises[msg.sender]);
-    return entrerpises[msg.sender];
-  }
-
-  function addBalanceEntreprise(uint256 amount) public returns (bool) {
-    require(entreprises[msg.sender].registered);
-    entreprises[msg.sender].balance += amount;
-    return true;
+    return entreprises[msg.sender];
   }
 
   //Project
-  function getProjectsByAddress(address ownerAddress) public view returns (Project[] memory){
-    require(users[ownerAddress].registered);
-    return projects[ownerAddress];
+  function getProjects(address owner) public view returns (Project[] memory){
+    return projects[owner];
   }
 
-  function getProjectByIdAndAddress(address ownerAddress, uint256 id) public view returns (Project memory) {
-    require(users[ownerAddress].registered);
-    Project[] memory ownerProjects = getProjectsByAddress(ownerAddress);
-    for (uint i = 0; i < ownerProjects.length; i++){
-      if (ownerProjects[i].id == id){
-        return ownerProjects[i];
+  function getProjectById(address owner, uint256 id) public view returns (Project memory) {
+    Project[] memory projects = getProjects(owner);
+    for (uint i = 0; i < projects.length; i++){
+      if (projects[i].id == id){
+        return projects[i];
       }
     }
-    return ownerProjects[0];
+    return projects[0];
   }
 
-  function createProject(string memory name, uint256 memory balance, string memory belongsTo, address[] memory contributorsAddresses, string memory link) public returns (Project memory){
-    require(users[msg.sender].registred);
+  function createProject(string memory name, address[] memory contributors, uint256 balance, bool belongsToUser, string memory link) public returns (Project memory){
+    require(users[msg.sender].registered);
     require(bytes(name).length > 0);
-    ProjectId++;
-    Project memory project = Project(ProjectId, name, balance, contributorsAddresses, msg.sender, belongsTo, link);
+    CptProject++;
+    Project memory project = Project(CptProject, name, msg.sender, contributors, balance, belongsToUser, link);
     projects[msg.sender].push(project);
     emit ProjectCreated(msg.sender, project);
     return project;
   }
 
-  function sponsorProject(address ownerAddress, uint256 projectId, uint256 amount) public returns (bool){
-    require(users[ownerAddress].registered);
-    for(uint i = 0; i < projects[ownerAddress].length; i++){
-      if(projects[ownerAddress][i].id == projectId){
+  function sponsorProject(address owner, uint256 projectId, uint256 amount) public returns (bool){
+    require(users[owner].registered);
+    for(uint i = 0; i < projects[owner].length; i++){
+      if(projects[owner][i].id == projectId){
         users[msg.sender].balance -= amount;
-        projects[ownerAddress][i].balance += amount;
+        projects[owner][i].balance += amount;
         return true;
       }
     }
@@ -135,27 +129,26 @@ contract BuildCollective is Ownable {
   }
 
   //Bountie
-  function getBountiesByProjectId(uint256 projectId) public view returns (Bountie[] memory) {
-    require(projectId <= ProjectId);
-    return bounties[projectId];
+  function getProjectBounties(uint256 id) public view returns (Bountie[] memory) {
+    return bounties[id];
   }
 
-  function openBountie(uint256 projectId, uint256 reward, string memory link) public returns (Bountie memory){
-    require (users[msg.sender].registred);
-    BountieId++;
-    Bountie memory bountie = Bountie(BountieId, reward, msg.sender, address(0), link, false);
+  function openBountie(address projectOwner, uint256 projectId, string memory link, uint256 reward) public returns (Bountie memory){
+    require(users[projectOwner].registered);
+    require(users[msg.sender].registered);
+    CptBountie++;
+    Bountie memory bountie = Bountie(CptBountie, msg.sender, address(0), link, reward, false);
     bounties[projectId].push(bountie);
     emit BountieOpened(msg.sender, bountie);
     return bountie;
   }
 
-  function closeBountie(uint256 projectId, uint256 bountieId, address payable solver) public returns (bool){
-    for(uint i = 0; i < bounties[projectId].length; i++){
-      if(bounties[projectId][i].id == bountieId){
-        require(!bounties[projectId][i].solver);
-        bounties[projectId][i].solved = true;
+  function closeBountie(uint256 projectId, uint256 bountieId, address payable solver) public payable returns (bool){
+    for (uint i = 0; i < bounties[projectId].length; i++){
+      if (bounties[projectId][i].id == bountieId) {
+        bounties[projectId][i].resolved = true;
         bounties[projectId][i].solver = solver;
-        return fixer.send(bounties[projectId][i].reward);
+        return solver.send(bounties[projectId][i].reward);
       }
     }
     return false;
